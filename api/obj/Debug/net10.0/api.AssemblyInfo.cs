d283bd67e34d7b6269de928696836feaa34deadb
JohanWidell <johan.widell@hotmail.se>
2026-05-10 18:52:45 +0200

    Added GitIgnore

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a4e19d6d78a4349e73d5a9b6fa6fc5005263ee5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f2a3ce7aaee709720f3bad00b48e02bd16d588")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
